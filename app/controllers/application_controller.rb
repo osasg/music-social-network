4933e53b7cd06f424b5c253a7b78299a0f4d1478
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-26 00:08:45 +0530

    authentication using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :password_confirmation]
+    devise_parameter_sanitizer.for(:sign_in) << [:email, :remember_me]
+  end
 end
